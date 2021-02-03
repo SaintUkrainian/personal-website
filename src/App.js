76ba841defa62854399d3a04bedaca9535b7637f
Denys Matsenko <idanchik47@gmail.com>
2021-02-03 21:25:21 +0200

    router transitions finally working

--- src/App.js
+++ src/App.js
@@ -4,17 +4,25 @@ import "./App.css";
 import Nav from "./components/nav/Nav";
 import {Route, Switch} from "react-router";
 import Home from "./paths/Home/Home";
+import {TransitionGroup} from "react-transition-group";
+import CSSTransition from "react-transition-group/CSSTransition";
 
 function App() {
 
     return (
         <div className="App">
             <Nav/>
-            <Switch>
-                <Route path={"/about"}><h1>Hey</h1></Route>
-                <Route exact path={"/"} component={Home}/>
-            </Switch>
-
+            <Route render={({location}) => (
+                <TransitionGroup>
+                    <CSSTransition key={location.key} timeout={700}
+                                   classNames={"paths"} mountOnEnter unMountOnExit>
+                        <Switch location={location}>
+                            <Route path={"/about"} component={Home}/>
+                            <Route exact path={"/"} component={Home}/>
+                        </Switch>
+                    </CSSTransition>
+                </TransitionGroup>
+            )}/>
         </div>
     );
 }
