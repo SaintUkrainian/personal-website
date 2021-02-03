e0bc2c6d12e34db4e312d252e8bd2780db365fea
Denys Matsenko <idanchik47@gmail.com>
2021-02-03 12:26:20 +0200

    anims added to socias, navlinks added

--- src/components/nav/Nav.js
+++ src/components/nav/Nav.js
@@ -9,9 +9,18 @@ function Nav() {
 
     const [open, setOpen] = useState(false);
 
+    const links = (
+        <div className="Links">
+            <h3>Home</h3>
+            <h3>About</h3>
+            <h3>Portfolio</h3>
+        </div>
+    );
+
     return (
         <div className={["Nav", open ? "open" : null].join(" ")}>
             <NavIcon openNav={() => setOpen(!open)} open={open}/>
+            {open ? links : null}
             <Socials open={open}/>
         </div>
     );
--- src/components/socials/Socials.js
+++ src/components/socials/Socials.js
@@ -3,17 +3,21 @@ import React from "react";
 import GithubIcon from "../icons/Github";
 import LinkedInIcon from "../icons/LinkedIn";
 import GmailIcon from "../icons/Gmail";
+import CSSTransition from "react-transition-group/CSSTransition";
 
 import "./Socials.css";
 
 const socials = (props) => {
+    
     return (
-        <div className={["Socials", props.open ? "open" : null].join(" ")}>
-            <GithubIcon />
-            <LinkedInIcon />
-            <GmailIcon />
-        </div>
+        <CSSTransition in={props.open} timeout={300} classNames={"icons"}>
+            <div className={"Socials"}>
+                <GithubIcon />
+                <LinkedInIcon />
+                <GmailIcon />
+            </div>
+        </CSSTransition>
     );
-}
+};
 
 export default socials;
